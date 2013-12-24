d2db38a7ce7a807dc6d5a3e04b1a0e5304185f64
Alex Howington <alex.howington@gmail.com>
2013-12-24 03:10:51 -0700

    Add basic Area model without kml_file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 WaterRightsListing::Application.routes.draw do
+  resources :areas
+
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131218071529) do
+ActiveRecord::Schema.define(version: 20131224073346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "areas", force: true do |t|
+    t.string   "name"
+    t.string   "color"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
