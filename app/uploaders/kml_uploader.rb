b513f8832cf24063f2bde81dff9d1f2487e1d66a
Alex Howington <alex.howington@gmail.com>
2014-01-04 22:02:09 -0700

    Only upload to S3 in production env

--- app/uploaders/kml_uploader.rb
+++ app/uploaders/kml_uploader.rb
@@ -7,8 +7,12 @@ class KmlUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
+  if Rails.env.development? or Rails.env.test?
+    storage :file
+  end
+  if Rails.env.production?
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,31 @@
-CarrierWave.configure do |config|
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
 
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['S3_KEY'],                        # required
-    :aws_secret_access_key  => ENV['S3_SECRET'],                     # required
-    :region                 => ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']  
-  # config.fog_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
-  
+    config.fog_credentials = {
+      :provider               => 'AWS',                        # required
+      :aws_access_key_id      => ENV['S3_KEY'],                        # required
+      :aws_secret_access_key  => ENV['S3_SECRET'],                     # required
+      :region                 => ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = ENV['S3_BUCKET_NAME']  
+    # config.fog_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+    
+  end
+end
+
+if Rails.env.development? or Rails.env.test?
+  CarrierWave.configure do |config|
+    config.cache_dir = "#{Rails.root}/tmp/uploads/dev"
+
+    # config.fog_credentials = {
+    #   :provider               => 'AWS',                        # required
+    #   :aws_access_key_id      => ENV['S3_KEY'],                        # required
+    #   :aws_secret_access_key  => ENV['S3_SECRET'],                     # required
+    #   :region                 => ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
+    # }
+    # config.fog_directory  = ENV['DEV_S3_BUCKET_NAME']  
+    # config.fog_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+    
+  end
 end
