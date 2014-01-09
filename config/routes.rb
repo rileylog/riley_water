0e611b113d5b47a23aee22f385e0bd0c0f11c318
Alex Howington <alex.howington@gmail.com>
2014-01-09 00:20:38 -0700

    Update navigation links, admin login route, and for sale/wanted active order

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ WaterRightsListing::Application.routes.draw do
   resources :areas
 
   root :to => "home#index"
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users, :controllers => {:registrations => "registrations"}, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :users
   get "/dashboard", to: "home#dashboard", as: :dashboard
   get "/new_home", to: "home#new_home", as: :new_home
