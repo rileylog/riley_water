3f789fd5a1e96e60a7097760e8401ac66dc090ab
Alex Howington <alex.howington@gmail.com>
2014-01-12 16:08:31 -0700

    Add basic static page layouts, routes and actions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,4 +17,49 @@ class StaticPagesController < ApplicationController
     gon.wanteds = Wanted.all
     gon.for_sales = ForSale.all
   end
+
+  def services
+  end
+
+  def diligence_claims
+  end
+
+  def proof_of_beneficial_use
+  end
+
+  def change_application
+  end
+
+  def temporary_change_application
+  end
+
+  def reports_of_conveyance
+  end
+
+  def dam_inspection
+  end
+
+  def fourty_year_plan
+  end
+
+  def extension_request
+  end
+
+  def due_diligence
+  end
+
+  def applications_to_appropriate
+  end
+
+  def non_use_application
+  end
+
+  def projects
+  end
+
+  def profile
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,20 @@ WaterRightsListing::Application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :users
   get "/dashboard", to: "static_pages#dashboard", as: :dashboard
+  get "/services", to: "static_pages#services", as: :services
+  get "/diligence-claims", to: "static_pages#diligence_claims", as: :diligence_claims
+  get "/proof-of-beneficial-use", to: "static_pages#proof_of_beneficial_use", as: :proof_of_beneficial_use
+  get "/change-application", to: "static_pages#change_application", as: :change_application
+  get "/temporary-change-applicaton", to: "static_pages#temporary_change_application", as: :temporary_change_application
+  get "/reports-of-conveyance", to: "static_pages#reports_of_conveyance", as: :reports_of_conveyance
+  get "/dam-inspection", to: "static_pages#dam_inspection", as: :dam_inspection
+  get "/fourty-year-plan", to: "static_pages#fourty_year_plan", as: :fourty_year_plan
+  get "/extension-request", to: "static_pages#extension_request", as: :extension_request
+  get "/due-diligence", to: "static_pages#due_diligence", as: :due_diligence
+  get "/applications-to-appropriate", to: "static_pages#applications_to_appropriate", as: :applications_to_appropriate
+  get "/non-use-application", to: "static_pages#non_use_application", as: :non_use_application
+  get "/projects", to: "static_pages#projects", as: :projects
+  get "/profile", to: "static_pages#profile", as: :profile
+  get "/contact", to: "static_pages#contact", as: :contact
   get "/buy-sell", to: "static_pages#buy_sell", as: :buy_sell
 end
